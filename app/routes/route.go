af9e31d18928b8e56b2ab17e16c1d6e4bf64370d
vysedh2000 <hengvysedh@gmail.com>
2026-03-29 22:59:30 +0700

    add prefix

--- app/routes/route.go
+++ app/routes/route.go
@@ -5,19 +5,26 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Router(router*gin.Engine) {
+func Router(router *gin.Engine) {
 	userController := controllers.NewUserController()
 	accController := controllers.NewAccController()
+
 	router.Use(gin.Recovery())
 
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "pong"})
+	
+
+	core := router.Group("/core")
+	{
+
+		core.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
 	})
+		core.GET("/accSummary/:id", accController.AccSummary)
 
-	router.GET("/accSummary/:id", accController.AccSummary)
+		core.POST("/user/create", userController.CreateNewUser)
 
-	router.POST("/user/create", userController.CreateNewUser)
-
-	router.POST("/acc/create", accController.CreateNewAccount)
-
+		core.POST("/acc/create", accController.CreateNewAccount)
+	}
 }
